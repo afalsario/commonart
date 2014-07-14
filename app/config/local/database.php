dc443b0e61f7dd138a2aa411bdca6285bccc0cbe
Ashley <a_falsario@yahoo.com>
2014-07-14 17:35:00 -0500

    adding database

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'common_art_db',
 			'username'  => 'admin',
-			'password'  => 'password',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
