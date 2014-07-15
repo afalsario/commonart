4af681488c36762ce4204acf521ac4c127328e09
Ashley <a_falsario@yahoo.com>
2014-07-15 15:32:25 -0500

    adding labels and info

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -3,6 +3,23 @@
 
 @section('content')
 
-This is the artist Profile page. 
+<h2>This is the artist Profile page.</h2>
+
+One artist by id
+<br>
+<br>
+<label>Name: </label>
+{{{ $profile->name }}}
+<br>
+<label>Title: </label>
+{{{ $profile->title }}}
+<br>
+<label>Weapon of Choice: </label>
+{{{ $profile->mediums }}}
+<br>
+<label>About me: </label>
+{{{ $profile->about_me }}}
+<br>
+
 
 @stop
