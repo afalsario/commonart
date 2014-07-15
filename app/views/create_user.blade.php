f163ec125440be1b7d78521dd6cc31c0d1f97002
Ashley <a_falsario@yahoo.com>
2014-07-15 10:12:54 -0500

    adding error messages

--- app/views/create_user.blade.php
+++ app/views/create_user.blade.php
@@ -1,16 +1,20 @@
 <html>
 <head>
-    <title></title>
+    <title>Registration</title>
 </head>
 <body>
+<h2>Registration Page</h2>
 
 {{ Form::open(array('action' => 'UsersController@store')) }}
 {{ Form::label('Username')}}
 {{ Form::text('username') }}
+{{ $errors->first('username', '<span class="help-block">:message</span></br>') }}
 {{ Form::label('Email Address') }}
 {{ Form::text('email') }}
+{{ $errors->first('email', '<span class="help-block">:message</span></br>') }}
 {{ Form::label('Password')}}
 {{ Form::password('password') }}
+{{ $errors->first('password', '<span class="help-block">:message</span></br>') }}
 {{ Form::submit('Submit') }}
 {{ Form::close() }}
 
