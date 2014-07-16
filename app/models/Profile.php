1e138f692384a4299cc5fe7c70ea266cd9db8be2
Ashley <a_falsario@yahoo.com>
2014-07-15 21:19:30 -0500

    added image upload function and modified rules

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -3,4 +3,21 @@
 class Profile extends Eloquent{
 
     protected $table = 'profiles';
+
+    protected $imgDir = 'img-upload';
+
+    public static $rules = 
+    [
+        'name' => 'required|max:100',
+        'title' => 'required|max:100',
+        'about_me' => 'required|max:1000',
+    ];
+
+    public function addUpLoadedImage($image)
+    {
+        $systemPath = public_path() . '/' . $this->imgDir . '/';
+        $imageName = $this->id . '-' . $image->getClientOriginalName();
+        $image->move($systemPath, $imageName);
+        $this->img_path = '/' . $this->imgDir . '/' . $imageName;
+    }
 }
