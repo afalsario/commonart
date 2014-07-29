3b8ee5604c56b972fcb5cf859789236a21681ec5
Alex Zuniga <azuniga90@yahoo.com>
2014-07-29 09:21:08 -0500

    Deleteing needless files

--- app/views/layouts/partials/login.blade.php
+++ app/views/layouts/partials/login.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h3 class="modal-title" id="myModalLabel">Have an Account?</h3>
+        <h3 class="modal-title" id="myModalLabel">Welcome Artists!</h3>
       </div>
       <div class="modal-body">
         <!-- begin code -->
